5b178cf4db0fe70165932a7d5f519b54f2436183
MissChurchill <misschurchill.dc@gmail.com>
2021-12-20 10:32:00 -0500

    Updated to fix errors

--- app/src/main/java/com/example/clickit/MainActivity.kt
+++ app/src/main/java/com/example/clickit/MainActivity.kt
@@ -10,6 +10,12 @@ import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.app.ActivityCompat.startActivityForResult
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
+import kotlin.math.roundToInt
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,43 +27,46 @@ class MainActivity : AppCompatActivity() {
             dispatchTakePictureIntent()
         }
     }
-}
 
-private val REQUEST_IMAGE_CAPTURE = 1
+    private val REQUEST_IMAGE_CAPTURE = 1
 
-private fun dispatchTakePictureIntent() {
-    val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-    try {
-        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
-    } catch (e: ActivityNotFoundException) {
-        // display error state to the user
-
-        //Get Thumbnail
-override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    super.onActivityResult(requestCode, resultCode, data)
-    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-
-        val imageBitmap = data?.extras?.get("data") as Bitmap
-
-        findViewById<ImageView>(R.id.imageView).setImageBitmap(imageBitmap
-
-        val image = InputImage.fromBitmap(imageBitmap, rotationDegrees: 0)
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
     }
-}
-var outputText = ''
-labeler.process(image)
-    .addOnSuccessListener { labels ->
-        // Task Completed successfully
-        Log.i("Dom", "successfully processed image"")
-                for (label in labels) {
-                    val text = label.text
-                    val confidence = (label.confidence*100).roundToInt().toString()
-                    val index = label.index
-                    val textView = findViewByID<Textview>(R.id.textView)
-                    outputText += "$text : $confidence %\n"
-                    textView.text = outputText
-                    Log.i("Dom", "LOOP" > [$TEXT]:$confidence"")
+
+    //Get Thumbnail
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+
+            findViewById<ImageView>(R.id.imageButton).setImageBitmap(
+                imageBitmap
+            )
+
+            val image = InputImage.fromBitmap(imageBitmap, 0)
+
+            val labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS)
+
+            var outputText = ""
+            labeler.process(image)
+                .addOnSuccessListener { labels ->
+                    // Task Completed successfully
+                    for (label in labels) {
+                        val text = label.text
+                        val confidence = (label.confidence * 100).roundToInt().toString()
+                        val index = label.index
+                        val textView = findViewById<TextView>(R.id.textView)
+                        outputText += "$text : $confidence %\n"
+                        textView.text = outputText
+                    }
                 }
+        }
     }
-
 }
